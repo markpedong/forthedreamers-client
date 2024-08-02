0ce7715678fd401621459968f5e1d1e253dfbbed
atlas <atlas.wbridges@gmail.com>
2024-08-02 17:18:17 +0800

    no message

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,28 @@
 /** @type {import('next').NextConfig} */
-import { fileURLToPath } from 'url';
-import path, { dirname } from 'path';
+import { fileURLToPath } from 'url'
+import path, { dirname } from 'path'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const nextConfig = {
-  output: "standalone",
-  sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')],
-    additionalData: `@import "./src/styles/_helpers.scss";`,
-  },
-};
+	output: 'standalone',
+	sassOptions: {
+		includePaths: [path.join(__dirname, 'styles')],
+		additionalData: `@import "./src/styles/_helpers.scss";`
+	},
+	images: {
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'res.cloudinary.com'
+			},
+      {
+				protocol: 'http',
+				hostname: 'res.cloudinary.com'
+			}
+		]
+	}
+}
 
-export default nextConfig;
+export default nextConfig
