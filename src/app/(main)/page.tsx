ec36b9a364f2cbd3a6da47c2d44d58228baaa3c9
atlas <atlas.wbridges@gmail.com>
2024-08-07 13:56:56 +0800

    test

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,32 +1,31 @@
-//@ts-nocheck
 // import { GoDotFill } from 'react-icons/go'
 // import { FaCircleDot } from 'react-icons/fa6'
-import { Poppins, Roboto_Condensed } from 'next/font/google'
-import Image from 'next/image'
-import { getProducts, getWebsiteData } from '@/api'
-import classNames from 'classnames'
+// import { Poppins, Roboto_Condensed } from 'next/font/google'
+// import Image from 'next/image'
+// import { getProducts, getWebsiteData } from '@/api'
+// import classNames from 'classnames'
 
-import Marquee from '@/components/marquee'
-import Testimonials from '@/components/testimonials'
-import styles from '@/styles/styles.module.scss'
+// import Marquee from '@/components/marquee'
+// import Testimonials from '@/components/testimonials'
+// import styles from '@/styles/styles.module.scss'
 
-import CityProduct from './components/city-product'
-import Products from './components/products'
+// import CityProduct from './components/city-product'
+// import Products from './components/products'
 
-const roboto = Roboto_Condensed({ weight: ['300', '800'], subsets: ['latin'] })
-const poppins = Poppins({ weight: ['400', '600', '800'], subsets: ['latin'] })
+// const roboto = Roboto_Condensed({ weight: ['300', '800'], subsets: ['latin'] })
+// const poppins = Poppins({ weight: ['400', '600', '800'], subsets: ['latin'] })
 
 const Home = async () => {
-  const [products, web] = await Promise.all([getProducts({}), getWebsiteData({})])
-  const website = web?.data
+  // const [products, web] = await Promise.all([getProducts({}), getWebsiteData({})])
+  // const website = web?.data
 
   return (
     <div>
-      <div
+      {/* <div
         className={classNames(styles.mainWrapper)}
         style={{
           backgroundImage: `url(${website?.landing_image1})`,
-        }}
+        }} 
       >
         {/* <div className={styles.timelessContainer}>
 					<span>timeless silhouettes</span>
@@ -35,12 +34,12 @@ const Home = async () => {
 						<FaCircleDot />
 						<GoDotFill />
 					</div>
-				</div> */}
-      </div>
+				</div> 
+      </div>*/}
       {/* <Products products={products?.data ?? []} />
-      <Marquee text={website?.marquee_text ?? ""} />
+      <Marquee text={website?.marquee_text ?? ''} />
       <div className={styles.dudeWrapper}>
-        <Image src={website?.landing_image2 ?? ""} alt="" height={1000} width={1000} />
+        <Image src={website?.landing_image2 ?? ''} alt="" height={1000} width={1000} />
       </div>
       <div className={styles.afterProductWrapper}>
         <div className={styles.header}>
@@ -50,7 +49,7 @@ const Home = async () => {
         <CityProduct products={products?.data ?? []} />
       </div>
       <div className={styles.dudeWrapper}>
-        <Image src={website?.landing_image3 ?? ""} alt="" height={1000} width={1000} />
+        <Image src={website?.landing_image3 ?? ''} alt="" height={1000} width={1000} />
       </div>
       <Testimonials /> */}
     </div>
