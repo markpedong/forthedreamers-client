d7cba982c8b4f6096b9a85473921691a1f3375d1
atlas <atlas.wbridges@gmail.com>
2024-08-07 14:04:19 +0800

    test

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,19 +1,19 @@
 // import { GoDotFill } from 'react-icons/go'
 // import { FaCircleDot } from 'react-icons/fa6'
-// import { Poppins, Roboto_Condensed } from 'next/font/google'
+import { Poppins, Roboto_Condensed } from 'next/font/google'
 // import Image from 'next/image'
 // import { getProducts, getWebsiteData } from '@/api'
-// import classNames from 'classnames'
+import classNames from 'classnames'
 
 // import Marquee from '@/components/marquee'
-// import Testimonials from '@/components/testimonials'
-// import styles from '@/styles/styles.module.scss'
+import Testimonials from '@/components/testimonials'
+import styles from '@/styles/styles.module.scss'
 
 // import CityProduct from './components/city-product'
 // import Products from './components/products'
 
-// const roboto = Roboto_Condensed({ weight: ['300', '800'], subsets: ['latin'] })
-// const poppins = Poppins({ weight: ['400', '600', '800'], subsets: ['latin'] })
+const roboto = Roboto_Condensed({ weight: ['300', '800'], subsets: ['latin'] })
+const poppins = Poppins({ weight: ['400', '600', '800'], subsets: ['latin'] })
 
 const Home = async () => {
   // const [products, web] = await Promise.all([getProducts({}), getWebsiteData({})])
@@ -21,11 +21,11 @@ const Home = async () => {
 
   return (
     <div>
-      {/* <div
+      <div
         className={classNames(styles.mainWrapper)}
         style={{
-          backgroundImage: `url(${website?.landing_image1})`,
-        }} 
+          // backgroundImage: `url(${website?.landing_image1})`,
+        }}
       >
         {/* <div className={styles.timelessContainer}>
 					<span>timeless silhouettes</span>
@@ -34,24 +34,24 @@ const Home = async () => {
 						<FaCircleDot />
 						<GoDotFill />
 					</div>
-				</div> 
-      </div>*/}
+				</div> */}
+      </div>
       {/* <Products products={products?.data ?? []} />
-      <Marquee text={website?.marquee_text ?? ''} />
+      <Marquee text={website?.marquee_text ?? ""} /> */}
       <div className={styles.dudeWrapper}>
-        <Image src={website?.landing_image2 ?? ''} alt="" height={1000} width={1000} />
+        {/* <Image src={website?.landing_image2 ?? ""} alt="" height={1000} width={1000} /> */}
       </div>
       <div className={styles.afterProductWrapper}>
         <div className={styles.header}>
           <span className={poppins.className}>FOR THE DREAMERS CITY</span>
           <span className={roboto.className}>Inspired by the vibrant aesthetics of the urban and the cozy spirit of a hometown.</span>
         </div>
-        <CityProduct products={products?.data ?? []} />
+        {/* <CityProduct products={products?.data ?? []} /> */}
       </div>
       <div className={styles.dudeWrapper}>
-        <Image src={website?.landing_image3 ?? ''} alt="" height={1000} width={1000} />
+        {/* <Image src={website?.landing_image3 ?? ""} alt="" height={1000} width={1000} /> */}
       </div>
-      <Testimonials /> */}
+      <Testimonials />
     </div>
   )
 }
