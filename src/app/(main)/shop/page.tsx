fc478cac9979511706917a00229b1af9e7e01656
atlas <atlas.wbridges@gmail.com>
2024-08-07 14:01:59 +0800

    test

--- src/app/(main)/shop/page.tsx
+++ src/app/(main)/shop/page.tsx
@@ -1,19 +1,19 @@
 import React from 'react'
-// import { Roboto_Condensed } from 'next/font/google'
+import { Roboto_Condensed } from 'next/font/google'
 // import Image from 'next/image'
 // import { getProducts } from '@/api'
 // import { TProductItem } from '@/api/types'
-// import classNames from 'classnames'
-// import { FaFilter } from 'react-icons/fa'
-// import { FaAngleDown, FaArrowRight } from 'react-icons/fa6'
+import classNames from 'classnames'
+import { FaFilter } from 'react-icons/fa'
+import { FaAngleDown, FaArrowRight } from 'react-icons/fa6'
 
-// import Header from '@/components/header'
-// import { PageTitle } from '@/components/page-components'
+import Header from '@/components/header'
+import { PageTitle } from '@/components/page-components'
 // import Product from '@/components/product'
 
 import styles from './styles.module.scss'
 
-// const roboto = Roboto_Condensed({ weight: ['300', '800'], subsets: ['latin'] })
+const roboto = Roboto_Condensed({ weight: ['300', '800'], subsets: ['latin'] })
 
 const Page = async () => {
   // const [products] = await Promise.all([getProducts({})])
@@ -22,7 +22,7 @@ const Page = async () => {
 
   return (
     <div className={styles.mainWrapper}>
-      {/* <Header arr={['HOME', 'SHOP', 'PRODUCTS']} />
+      <Header arr={['HOME', 'SHOP', 'PRODUCTS']} />
       <PageTitle title="PRODUCTS" />
       <div className={classNames(styles.filterContainer, roboto.className)}>
         <div>
@@ -32,10 +32,10 @@ const Page = async () => {
         <div>
           <span className={styles.sort}>ALPHABETICALLY, A-Z</span>
           <FaAngleDown />
-          <span>{products?.data?.length} PRODUCTS</span>
+          {/* <span>{products?.data?.length} PRODUCTS</span> */}
         </div>
       </div>
-      <div className={styles.productWrapper}>{products?.data?.map(item => <Product product={item} />)}</div>
+      {/* <div className={styles.productWrapper}>{products?.data?.map(item => <Product product={item} />)}</div> */}
       <div className={classNames(styles.pagination, roboto.className)}>
         <span>1</span>
         <span>2</span>
@@ -48,16 +48,16 @@ const Page = async () => {
       <div className={styles.crowdWrapper}>
         <span className={styles.crowdTitle}>CROWD FAVORITES</span>
         <div className={styles.crowdItemsContainer}>
-          <div>
+          {/* <div>
             <Image src={first?.images?.[0]} width={500} height={500} alt="img1" />
             <span>{first?.name}</span>
           </div>
           <div>
             <Image src={second?.images?.[0]} width={500} height={500} alt="img1" />
             <span>{second?.name}</span>
-          </div>
+          </div> */}
         </div>
-      </div> */}
+      </div>
     </div>
   )
 }
