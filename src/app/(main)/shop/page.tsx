9f137fb99c652d44e7cfab3ecf836e9b942eb8f5
atlas <atlas.wbridges@gmail.com>
2024-08-07 14:11:50 +0800

    1213123213213213121

--- src/app/(main)/shop/page.tsx
+++ src/app/(main)/shop/page.tsx
@@ -1,24 +1,24 @@
 import React from 'react'
 import { Roboto_Condensed } from 'next/font/google'
-// import Image from 'next/image'
-// import { getProducts } from '@/api'
-// import { TProductItem } from '@/api/types'
+import Image from 'next/image'
+import { getProducts } from '@/api'
+import { TProductItem } from '@/api/types'
 import classNames from 'classnames'
 import { FaFilter } from 'react-icons/fa'
 import { FaAngleDown, FaArrowRight } from 'react-icons/fa6'
 
 import Header from '@/components/header'
 import { PageTitle } from '@/components/page-components'
-// import Product from '@/components/product'
+import Product from '@/components/product'
 
 import styles from './styles.module.scss'
 
 const roboto = Roboto_Condensed({ weight: ['300', '800'], subsets: ['latin'] })
 
 const Page = async () => {
-  // const [products] = await Promise.all([getProducts({})])
-  // const first: TProductItem = products?.[0] ?? {}
-  // const second: TProductItem = products?.[1] ?? {}
+  const [products] = await Promise.all([getProducts({})])
+  const first: TProductItem = products?.[0] ?? {}
+  const second: TProductItem = products?.[1] ?? {}
 
   return (
     <div className={styles.mainWrapper}>
@@ -32,10 +32,10 @@ const Page = async () => {
         <div>
           <span className={styles.sort}>ALPHABETICALLY, A-Z</span>
           <FaAngleDown />
-          {/* <span>{products?.data?.length} PRODUCTS</span> */}
+          <span>{products?.data?.length} PRODUCTS</span>
         </div>
       </div>
-      {/* <div className={styles.productWrapper}>{products?.data?.map(item => <Product product={item} />)}</div> */}
+      <div className={styles.productWrapper}>{products?.data?.map(item => <Product product={item} />)}</div>
       <div className={classNames(styles.pagination, roboto.className)}>
         <span>1</span>
         <span>2</span>
@@ -48,14 +48,14 @@ const Page = async () => {
       <div className={styles.crowdWrapper}>
         <span className={styles.crowdTitle}>CROWD FAVORITES</span>
         <div className={styles.crowdItemsContainer}>
-          {/* <div>
+          <div>
             <Image src={first?.images?.[0]} width={500} height={500} alt="img1" />
             <span>{first?.name}</span>
           </div>
           <div>
             <Image src={second?.images?.[0]} width={500} height={500} alt="img1" />
             <span>{second?.name}</span>
-          </div> */}
+          </div>
         </div>
       </div>
     </div>
