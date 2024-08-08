39d1e027a4ec016d6f4f3b44717cd1d0e791e602
atlas <atlas.wbridges@gmail.com>
2024-08-08 11:00:09 +0800

    - fix pagination

--- src/app/(main)/collection/collection.tsx
+++ src/app/(main)/collection/collection.tsx
@@ -1,7 +1,7 @@
 'use client'
 
-import { FC, memo, useState } from 'react'
-import { getCollections } from '@/api'
+import { FC, memo, useEffect, useState } from 'react'
+import { usePathname, useRouter } from 'next/navigation'
 import { TCollectionItem } from '@/api/types'
 import { useAppSelector } from '@/redux/store'
 
@@ -11,8 +11,9 @@ import Category from './components/category'
 import styles from './styles.module.scss'
 
 const Collection: FC<{ data: TCollectionItem[] }> = ({ data }) => {
+  const pathname = usePathname()
+  const router = useRouter()
   const { collection_length, default_pageSize } = useAppSelector(state => state.appData.website)
-  const [collections, setCollections] = useState(data)
   const [currentPage, setCurrentPage] = useState(1)
   const totalPages = Math.ceil(collection_length / default_pageSize)
   const validCurrentPage = Math.max(1, Math.min(currentPage, totalPages))
@@ -25,14 +26,15 @@ const Collection: FC<{ data: TCollectionItem[] }> = ({ data }) => {
     if (type === 'current' && page !== undefined) newPage = page
 
     setCurrentPage(newPage)
-
-    const res = await getCollections({ page: newPage })
-    setCollections(res?.data ?? [])
   }
-  
+
+  useEffect(() => {
+    router.push(`${pathname}?page=${currentPage}`)
+  }, [currentPage, pathname, router])
+
   return (
     <>
-      <div className={styles.categoryWrapper}>{collections?.map(collection => <Category data={collection} key={collection?.id} />)}</div>
+      <div className={styles.categoryWrapper}>{data?.map(collection => <Category data={collection} key={collection?.id} />)}</div>
       <Pagination
         currentPage={validCurrentPage}
         pageTotal={collection_length / default_pageSize < 1 ? 1 : collection_length / default_pageSize}
