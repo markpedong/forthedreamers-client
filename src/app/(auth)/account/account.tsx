5f042c57ebf2c0525ef7c7b9e2cdc1d216add517
markpedong <mark.pedong01@gmail.com>
2024-09-29 23:35:01 +0800

    Revert "import"
    This reverts commit 2e67c0ef5ee94a12c7cd9d94a8322e9cf24dd975.

--- src/app/(auth)/account/account.tsx
+++ src/app/(auth)/account/account.tsx
@@ -1,38 +1,40 @@
 'use client'
 
-import classNames from 'classnames'
-import { Roboto_Condensed } from 'next/font/google'
 import React from 'react'
-import styles from './styles.module.scss'
+import { Roboto_Condensed } from 'next/font/google'
 import Image from 'next/image'
-import { Input } from '../../../components/ui/input'
-import { Label } from '../../../components/ui/label'
-import { PageTitle } from '../../../components/page-components'
+import classNames from 'classnames'
+
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import { PageTitle } from '@/components/page-components'
+
+import styles from './styles.module.scss'
 
 const roboto = Roboto_Condensed({ weight: ['200', '300', '400', '500', '600', '800'], subsets: ['latin'] })
 
 const AccountPage = () => {
-	return (
-		<div className={classNames(styles.mainWrapper, roboto.className)}>
-			<PageTitle title="account" medium className="!capitalize" />
-			<div className={styles.details}>
-				<Image src={'/assets/images/dog.jpg'} alt="dog" width={100} height={100} />
-				<div className={styles.details__container}>
-					<div className={styles.details__label}>Full Name</div>
-					<div className={styles.twoInputContainer}>
-						<div className="grid w-full max-w-sm items-center gap-1.5">
-							<Label htmlFor="first_name">First Name</Label>
-							<Input type="first_name" id="first_name" placeholder="eg: John" value="John" />
-						</div>{' '}
-						<div className="grid w-full max-w-sm items-center gap-1.5">
-							<Label htmlFor="last_name">Last Name</Label>
-							<Input type="last_name" id="last_name" placeholder="eg: Doe" value="Doe" />
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	)
+  return (
+    <div className={classNames(styles.mainWrapper, roboto.className)}>
+      <PageTitle title="account" medium className="!capitalize" />
+      <div className={styles.details}>
+        <Image src={'/assets/images/dog.jpg'} alt="dog" width={100} height={100} />
+        <div className={styles.details__container}>
+          <div className={styles.details__label}>Full Name</div>
+          <div className={styles.twoInputContainer}>
+            <div className="grid w-full max-w-sm items-center gap-1.5">
+              <Label htmlFor="first_name">First Name</Label>
+              <Input type="first_name" id="first_name" placeholder="eg: John" value="John" />
+            </div>{' '}
+            <div className="grid w-full max-w-sm items-center gap-1.5">
+              <Label htmlFor="last_name">Last Name</Label>
+              <Input type="last_name" id="last_name" placeholder="eg: Doe" value="Doe" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default AccountPage
