c66516d9a88b3414ecea0e9a8f12354f9d83317c
markpedong <mark.pedong01@gmail.com>
2024-09-29 23:41:37 +0800

    import

--- src/app/(main)/shop/[id]/page.tsx
+++ src/app/(main)/shop/[id]/page.tsx
@@ -1,17 +1,17 @@
-import { getProductDetails, getProducts, getVariations } from '@/api'
+import { getProductDetails, getVariations } from '@/api'
 
 import Product from './product'
 
-export async function generateStaticParams() {
-  const products = await getProducts({})
+// export async function generateStaticParams() {
+//   const products = await getProducts({})
 
-  const params =
-    products?.data?.map(product => ({
-      id: product.id,
-    })) || []
+//   const params =
+//     products?.data?.map(product => ({
+//       id: product.id,
+//     })) || []
 
-  return params
-}
+//   return params
+// }
 
 const Page = async ({ params }: { params: { id: string } }) => {
   const [product, variations] = await Promise.all([getProductDetails({ product_id: params.id }), getVariations({ product_id: params.id })])
