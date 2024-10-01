484fe5884ac239ab79a9246ec3d67356b7f78f77
atlas <atlas.wbridges@gmail.com>
2024-10-01 16:39:22 +0800

    development.

--- src/app/(auth)/account/account.tsx
+++ src/app/(auth)/account/account.tsx
@@ -1,36 +1,79 @@
 'use client'
 
-import React from 'react'
-import { Roboto_Condensed } from 'next/font/google'
-import Image from 'next/image'
-import classNames from 'classnames'
+import { useState } from 'react'
+import { FaAddressCard, FaRegCreditCard, FaShoppingCart, FaStar, FaUser } from 'react-icons/fa'
 
-import { Input } from '@/components/ui/input'
-import { Label } from '@/components/ui/label'
+import { Command, CommandGroup, CommandItem, CommandList, CommandSeparator } from '@/components/ui/command'
 import { PageTitle } from '@/components/page-components'
 
+import Address from './address'
+import Profile from './profile'
 import styles from './styles.module.scss'
 
-const roboto = Roboto_Condensed({ weight: ['200', '300', '400', '500', '600', '800'], subsets: ['latin'] })
-
 const AccountPage = () => {
+  const [currTab, setCurrTab] = useState(1)
+
   return (
-    <div className={classNames(styles.mainWrapper, roboto.className)}>
-      <PageTitle title="account" medium className="!capitalize" />
-      <div className={styles.details}>
-        <Image src={'/assets/images/dog.jpg'} alt="dog" width={100} height={100} />
-        <div className={styles.details__container}>
-          <div className={styles.details__label}>Full Name</div>
-          <div className={styles.twoInputContainer}>
-            <div className="grid w-full max-w-sm items-center gap-1.5">
-              <Label htmlFor="first_name">First Name</Label>
-              <Input type="first_name" id="first_name" placeholder="eg: John" value="John" />
-            </div>{' '}
-            <div className="grid w-full max-w-sm items-center gap-1.5">
-              <Label htmlFor="last_name">Last Name</Label>
-              <Input type="last_name" id="last_name" placeholder="eg: Doe" value="Doe" />
-            </div>
-          </div>
+    <div className={styles.mainWrapper}>
+      <PageTitle title="account" medium className="!text-[1.5rem] !capitalize" />
+      <div className={styles.menuWrapper}>
+        <Command className="max-w-[200px] rounded-lg border shadow-md">
+          <CommandList>
+            <CommandGroup heading="Settings">
+              <CommandItem
+                onClick={() => {
+                  console.log('1')
+                  setCurrTab(1)
+                }}
+              >
+                <FaUser className="mr-2 h-4 w-4" />
+                <span>Profile</span>
+              </CommandItem>
+              <CommandItem
+                onClick={() => {
+                  console.log('1')
+                  setCurrTab(2)
+                }}
+              >
+                <FaAddressCard className="mr-2 h-4 w-4" />
+                <span>Address</span>
+              </CommandItem>
+              <CommandItem
+                onClick={() => {
+                  console.log('1')
+                  setCurrTab(3)
+                }}
+              >
+                <FaRegCreditCard className="mr-2 h-4 w-4" />
+                <span>Payment Methods</span>
+              </CommandItem>
+            </CommandGroup>
+            <CommandSeparator />
+            <CommandGroup heading="Other Details">
+              <CommandItem
+                onClick={() => {
+                  console.log('1')
+                  setCurrTab(4)
+                }}
+              >
+                <FaShoppingCart className="mr-2 h-4 w-4" />
+                <span>Orders</span>
+              </CommandItem>
+              <CommandItem
+                onClick={() => {
+                  console.log('1')
+                  setCurrTab(5)
+                }}
+              >
+                <FaStar className="mr-2 h-4 w-4" />
+                <span>Reviews</span>
+              </CommandItem>
+            </CommandGroup>
+          </CommandList>
+        </Command>
+        <div className={styles.detailsWrapper}>
+          {currTab === 1 && <Profile />}
+          {currTab === 2 && <Address />}
         </div>
       </div>
     </div>
