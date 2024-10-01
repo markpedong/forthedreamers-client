d27f360bde61e303fb98b7626ebdc12fe5403314
atlas <atlas.wbridges@gmail.com>
2024-10-01 16:39:22 +0800

    development.

--- src/api/http.ts
+++ src/api/http.ts
@@ -45,6 +45,7 @@ const upload = async <T>(url: string, data: File): Promise<ApiResponse<T>> => {
 
 const post = async <T>(url: string, data = {}): Promise<ApiResponse<T>> => {
   const token = getLocalStorage('token')
+  console.log(process.env.NEXT_PUBLIC_DOMAIN)
   const apiResponse = await fetch(`${process.env.NEXT_PUBLIC_DOMAIN}${url}`, {
     method: 'POST',
     headers: {
