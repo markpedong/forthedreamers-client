a6af96fd9f0c381e59ae274f6c48ed50cb2915fd
atlas <atlas.wbridges@gmail.com>
2024-10-01 18:07:27 +0800

    asd

--- src/app/(auth)/account/account.tsx
+++ src/app/(auth)/account/account.tsx
@@ -1,17 +1,22 @@
 'use client'
 
 import { useState } from 'react'
+import Image from 'next/image'
+import { LoadingOutlined, PlusOutlined } from '@ant-design/icons'
+import { GetProp, message, Upload, UploadProps } from 'antd'
 import { FaAddressCard, FaRegCreditCard, FaShoppingCart, FaStar, FaUser } from 'react-icons/fa'
 
 import { Command, CommandGroup, CommandItem, CommandList, CommandSeparator } from '@/components/ui/command'
 import { PageTitle } from '@/components/page-components'
 
+import styles from '../styles.module.scss'
 import Address from './address'
 import Orders from './orders'
 import PaymentMethods from './payment-methods'
 import Profile from './profile'
 import Reviews from './reviews'
-import styles from '../styles.module.scss'
+
+type FileType = Parameters<GetProp<UploadProps, 'beforeUpload'>>[0]
 
 const menus = [
   {
@@ -48,6 +53,27 @@ const menus = [
 
 const AccountPage = () => {
   const [currTab, setCurrTab] = useState(1)
+  const [loading, setLoading] = useState(false)
+  const [imageUrl, setImageUrl] = useState<string>()
+
+  const uploadButton = (
+    <button style={{ border: 0, background: 'none' }} type="button">
+      {loading ? <LoadingOutlined /> : <PlusOutlined />}
+      <div style={{ marginTop: 8 }}>Upload</div>
+    </button>
+  )
+
+  const beforeUpload = (file: FileType) => {
+    const isJpgOrPng = file.type === 'image/jpeg' || file.type === 'image/png'
+    if (!isJpgOrPng) {
+      message.error('You can only upload JPG/PNG file!')
+    }
+    const isLt2M = file.size / 1024 / 1024 < 2
+    if (!isLt2M) {
+      message.error('Image must smaller than 2MB!')
+    }
+    return isJpgOrPng && isLt2M
+  }
 
   return (
     <div className={styles.mainWrapper}>
@@ -55,6 +81,35 @@ const AccountPage = () => {
       <div className={styles.menuWrapper}>
         <Command className="max-w-[200px] rounded-lg border shadow-md">
           <CommandList>
+            <CommandGroup heading="Display Picture">
+              <Upload
+                className="flex justify-center"
+                listType="picture-circle"
+                name="avatar"
+                showUploadList={false}
+                beforeUpload={beforeUpload}
+                action={async e => {
+                  setLoading(true)
+                  setImageUrl('')
+
+                  try {
+                    // const res = await uploadImages(e)
+                    // setImageUrl(res?.data?.url)
+
+                    // return res?.data?.url
+                    return ''
+                  } finally {
+                    setLoading(false)
+                  }
+                }}
+              >
+                {imageUrl ? (
+                  <Image priority className={styles.profileImage} src={imageUrl} alt="avatar" width={100} height={100} />
+                ) : (
+                  uploadButton
+                )}
+              </Upload>
+            </CommandGroup>
             <CommandGroup heading="Settings">
               {menus?.slice(0, 3)?.map(menu => (
                 <CommandItem className="cursor-pointer" key={menu.id} onSelect={() => setCurrTab(menu.id)}>
