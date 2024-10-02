f983b4cda6e875645da1c613b976e3a733692f89
atlas <atlas.wbridges@gmail.com>
2024-10-02 16:19:58 +0800

    added use client && dynamic rendering

--- src/app/(main)/shop/[id]/components/care-guide/index.tsx
+++ src/app/(main)/shop/[id]/components/care-guide/index.tsx
@@ -1,44 +1,44 @@
 'use client'
 
-import { CARE_GUIDE_ANSWERS } from '@/app/(main)/constants'
-import { CARE_GUIDE } from '@/app/(main)/constants/enums'
-import Drawer from '@/components/drawer'
-import { DynamicListAnswers } from '@/components/dynamic-import'
 import { useLockBodyScroll } from '@uidotdev/usehooks'
 import classNames from 'classnames'
 import { roboto } from 'public/fonts'
 import { FC, useState } from 'react'
 import { IoIosCloseCircle } from 'react-icons/io'
-import Returns from '../returns'
-import Shipping from '../shipping'
+
+import { CARE_GUIDE_ANSWERS } from '@/app/(main)/constants'
+import { CARE_GUIDE } from '@/app/(main)/constants/enums'
+import Drawer from '@/components/drawer'
+import { DynamicListAnswers, DynamicReturns, DynamicShipping } from '@/components/dynamic-import'
+
 import styles from './styles.module.scss'
 
 const CareGuide: FC<{ setOpenCareGuide: () => void; activeTab: string }> = ({ setOpenCareGuide, activeTab }) => {
-	const [selectedTab, setSelectedTab] = useState(activeTab)
-	const tabs = ['care guide', 'shipping', 'returns']
+  const [selectedTab, setSelectedTab] = useState(activeTab)
+  const tabs = ['care guide', 'shipping', 'returns']
 
-	useLockBodyScroll()
+  useLockBodyScroll()
 
-	return (
-		<Drawer>
-			<div className={classNames(styles.header, roboto.className)}>
-				<span>PRODUCT INFORMATION</span>
-				<IoIosCloseCircle onClick={setOpenCareGuide} />
-			</div>
-			<div className={classNames(styles.tabs, roboto.className)}>
-				{tabs?.map(q => (
-					<span key={q} onClick={() => setSelectedTab(q)} data-isActive={q === selectedTab}>
-						{q}
-					</span>
-				))}
-			</div>
-			<div className={styles.content}>
-				{selectedTab === CARE_GUIDE.CARE_GUIDE && <DynamicListAnswers answers={CARE_GUIDE_ANSWERS} />}
-				{selectedTab === CARE_GUIDE.SHIPPING && <Shipping />}
-				{selectedTab === CARE_GUIDE.RETURNS && <Returns />}
-			</div>
-		</Drawer>
-	)
+  return (
+    <Drawer>
+      <div className={classNames(styles.header, roboto.className)}>
+        <span>PRODUCT INFORMATION</span>
+        <IoIosCloseCircle onClick={setOpenCareGuide} />
+      </div>
+      <div className={classNames(styles.tabs, roboto.className)}>
+        {tabs?.map(q => (
+          <span key={q} onClick={() => setSelectedTab(q)} data-isActive={q === selectedTab}>
+            {q}
+          </span>
+        ))}
+      </div>
+      <div className={styles.content}>
+        {selectedTab === CARE_GUIDE.CARE_GUIDE && <DynamicListAnswers answers={CARE_GUIDE_ANSWERS} />}
+        {selectedTab === CARE_GUIDE.SHIPPING && <DynamicShipping />}
+        {selectedTab === CARE_GUIDE.RETURNS && <DynamicReturns />}
+      </div>
+    </Drawer>
+  )
 }
 
 export default CareGuide
