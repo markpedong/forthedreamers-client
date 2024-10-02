e5ccb766656b801410e31a7e8b804426e095fbd2
atlas <atlas.wbridges@gmail.com>
2024-10-02 13:24:46 +0800

    optimized some codes.

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,15 +1,35 @@
 'use client'
 
+import classNames from 'classnames'
+import { motion } from 'framer-motion'
 import Image from 'next/image'
 import { useRouter } from 'next/navigation'
-import classNames from 'classnames'
 
 import InputWithLabel from '@/components/inputWithLabel'
+import { Form } from '@/components/ui/form'
+import { useProfileSchema } from '@/hooks/useProfileSchema'
 
 import styles from '../styles.module.scss'
 
 const Signup = () => {
   const router = useRouter()
+  const { form, handleSubmit, register, errors } = useProfileSchema({
+    username: '',
+    phone: '',
+    confirm_password: '',
+    password: '',
+    email: '',
+    first_name: '',
+    last_name: '',
+  })
+
+  const onSubmitForm = data => {
+    console.log(data)
+  }
+
+  const onError = (errors: any) => {
+    console.log(errors)
+  }
 
   return (
     <div className={styles.registerWrapper}>
@@ -19,19 +39,77 @@ const Signup = () => {
       <div className={styles.formContainer}>
         <h1>Create New Account 👋</h1>
         <span className={styles.subHeader}>Today is a new day. It's your day. You shape it. Sign in to start managing your projects.</span>
-        <div className="flex w-full gap-2">
-          <InputWithLabel key="first_name" label="First Name" placeholder="John" type="text" />
-          <InputWithLabel key="last_name" label="Last Name" placeholder="Mayer" type="text" />
-        </div>
-        <div className="flex w-full gap-2">
-          <InputWithLabel key="username" label="Username" placeholder="johnmayer" type="text" />
-          <InputWithLabel key="password" label="Password" placeholder="Atleast 8 characters" type="password" />
-        </div>
-        <InputWithLabel key="email" label="Email" placeholder="Example@gmail.com" type="email" />
-        <span className={styles.forgot} onClick={() => router.push('/forgot-password')}>
-          Forgot Password?
-        </span>
-        <div className={styles.signInBtn}>Sign Up</div>
+        <Form {...form}>
+          <form className="w-full" onSubmit={handleSubmit(onSubmitForm, onError)}>
+            <div className="flex w-full gap-2">
+              <InputWithLabel
+                key="first_name"
+                label="First Name"
+                placeholder="John"
+                type="text"
+                form={form}
+                err={errors?.first_name?.message}
+                {...register('first_name')}
+              />
+              <InputWithLabel
+                key="last_name"
+                label="Last Name"
+                placeholder="Mayer"
+                type="text"
+                form={form}
+                err={errors?.last_name?.message}
+                {...register('last_name')}
+              />
+            </div>
+            <div className="flex w-full gap-2">
+              <InputWithLabel
+                key="email"
+                label="Email"
+                placeholder="Example@gmail.com"
+                type="email"
+                form={form}
+                err={errors?.email?.message}
+                {...register('email')}
+              />
+              <InputWithLabel
+                key="username"
+                label="Username"
+                placeholder="johnmayer"
+                type="text"
+                form={form}
+                err={errors?.username?.message}
+                {...register('username')}
+              />
+            </div>
+            <div className="flex w-full gap-2">
+              <InputWithLabel
+                key="password"
+                label="Password"
+                placeholder="Atleast 8 characters"
+                type="password"
+                form={form}
+                err={errors?.password?.message}
+                {...register('password')}
+              />
+              <InputWithLabel
+                key="confirm_password"
+                label="Confirm Password"
+                placeholder="Atleast 8 characters"
+                type="password"
+                form={form}
+                err={errors?.confirm_password?.message}
+                {...register('confirm_password')}
+              />
+            </div>
+
+            <span className={styles.forgot} onClick={() => router.push('/forgot-password')}>
+              Forgot Password?
+            </span>
+            <motion.button type="submit" className={styles.signInBtn}>
+              Sign Up
+            </motion.button>
+          </form>
+        </Form>
         <div className="relative flex w-full items-center py-5">
           <div className="flex-grow border-t border-gray-400"></div>
           <span className="mx-4 flex-shrink text-gray-400">Or</span>
