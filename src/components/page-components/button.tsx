ac4b4da0df1997199463ba0c1131cbc1457fcadf
atlas <atlas.wbridges@gmail.com>
2024-10-03 17:50:41 +0800

    oauth login.

--- src/components/page-components/button.tsx
+++ src/components/page-components/button.tsx
@@ -1,9 +1,20 @@
 import Image from 'next/image'
 
 export const GoogleButton = () => {
+  const handleLogin = () => {
+    const popup = window.open('http://localhost:6601/public/googleLogin', 'Google Login', 'width=600,height=600')
+
+    const timer = setInterval(() => {
+      if (popup?.closed) {
+        clearInterval(timer)
+        console.log('Popup closed')
+      }
+    }, 1000)
+  }
+
   return (
     <div className="mb-2 flex w-full items-center justify-center bg-[#F3F9FA]">
-      <button className="flex items-center gap-2 px-6 py-2 text-xs text-[#313957]" onClick={() => window.open(`${process.env.NEXT_PUBLIC_DOMAIN}/public/googleLogin`)}>
+      <button className="flex items-center gap-2 px-6 py-2 text-xs text-[#313957]" onClick={handleLogin}>
         <Image src={'/assets/google.svg'} width={20} height={20} alt="google_logo" />
         <span>Sign in with Google</span>
       </button>
