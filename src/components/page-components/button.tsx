c48d444dc6dc5d24537815ebd3e5a960e65f2e59
atlas <atlas.wbridges@gmail.com>
2024-10-03 17:04:02 +0800

    asd

--- src/components/page-components/button.tsx
+++ src/components/page-components/button.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export const GoogleButton = () => {
   return (
     <div className="mb-2 flex w-full items-center justify-center bg-[#F3F9FA]">
-      <button className="flex items-center gap-2 px-6 py-2 text-xs text-[#313957]">
+      <button className="flex items-center gap-2 px-6 py-2 text-xs text-[#313957]" onClick={() => window.open(`${process.env.NEXT_PUBLIC_DOMAIN}/public/googleLogin`)}>
         <Image src={'/assets/google.svg'} width={20} height={20} alt="google_logo" />
         <span>Sign in with Google</span>
       </button>
