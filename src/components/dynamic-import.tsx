7d8c59551264e89dbb872935502ea30a03277e60
atlas <atlas.wbridges@gmail.com>
2024-10-10 16:11:33 +0800

    wq

--- src/components/dynamic-import.tsx
+++ src/components/dynamic-import.tsx
@@ -1,46 +1,46 @@
 import dynamic from 'next/dynamic'
 
-const DProduct = dynamic(() => import('@/components/product/index'), { loading: () => <></> })
+const Product = dynamic(() => import('@/components/product/index'), { loading: () => <></> })
 
-const DProducts = dynamic(() => import('../app/(main)/components/products/index'), { loading: () => <></> })
+const Products = dynamic(() => import('../app/(main)/components/products/index'), { loading: () => <></> })
 
-const DCityProduct = dynamic(() => import('../app/(main)/components/city-product/index'), { loading: () => <></> })
+const CityProduct = dynamic(() => import('../app/(main)/components/city-product/index'), { loading: () => <></> })
 
-const DynamicCareGuide = dynamic(() => import('../app/(main)/shop/[id]/components/care-guide'), { loading: () => <></> })
+const CareGuide = dynamic(() => import('../app/(main)/shop/[id]/components/care-guide'), { loading: () => <></> })
 
-const DynamicShipping = dynamic(() => import('../app/(main)/shop/[id]/components/shipping'), { loading: () => <></> })
+const Shipping = dynamic(() => import('../app/(main)/shop/[id]/components/shipping'), { loading: () => <></> })
 
-const DynamicReturns = dynamic(() => import('../app/(main)/shop/[id]/components/returns'), { loading: () => <></> })
+const Returns = dynamic(() => import('../app/(main)/shop/[id]/components/returns'), { loading: () => <></> })
 
-const DynamicListAnswers = dynamic(() => import('../components/page-components/index').then(m => m.ListAnswers), { loading: () => <></> })
+const ListAnswers = dynamic(() => import('../components/page-components/index').then(m => m.ListAnswers), { loading: () => <></> })
 
-const DynamicCart = dynamic(() => import('../components/navbar/components/cart'), { loading: () => <></> })
+const Cart = dynamic(() => import('../components/navbar/components/cart'), { loading: () => <></> })
 
-const DynamicSearch = dynamic(() => import('../components/navbar/components/search'), { loading: () => <></> })
+const Search = dynamic(() => import('../components/navbar/components/search'), { loading: () => <></> })
 
-const DynamicProfile = dynamic(() => import('../app/(auth)/account/profile'), { loading: () => <></>, ssr: false })
+const Profile = dynamic(() => import('../app/(auth)/account/profile'), { loading: () => <></>, ssr: false })
 
-const DynamicAddress = dynamic(() => import('../app/(auth)/account/address'), { loading: () => <></>, ssr: false })
+const Address = dynamic(() => import('../app/(auth)/account/address'), { loading: () => <></>, ssr: false })
 
-const DynamicOrders = dynamic(() => import('../app/(auth)/account/orders'), { loading: () => <></>, ssr: false })
+const Orders = dynamic(() => import('../app/(auth)/account/orders'), { loading: () => <></>, ssr: false })
 
-const DynamicPaymentMethods = dynamic(() => import('../app/(auth)/account/payment-methods'), { loading: () => <></>, ssr: false })
+const PaymentMethods = dynamic(() => import('../app/(auth)/account/payment-methods'), { loading: () => <></>, ssr: false })
 
-const DynamicReviews = dynamic(() => import('../app/(auth)/account/reviews'), { loading: () => <></>, ssr: false })
+const Reviews = dynamic(() => import('../app/(auth)/account/reviews'), { loading: () => <></>, ssr: false })
 
 export {
-  DProduct,
-  DProducts,
-  DCityProduct,
-  DynamicCareGuide,
-  DynamicListAnswers,
-  DynamicReturns,
-  DynamicShipping,
-  DynamicCart,
-  DynamicSearch,
-  DynamicProfile,
-  DynamicAddress,
-  DynamicOrders,
-  DynamicPaymentMethods,
-  DynamicReviews,
+  Product,
+  Products,
+  CityProduct,
+  CareGuide,
+  ListAnswers,
+  Returns,
+  Shipping,
+  Cart,
+  Search,
+  Profile,
+  Address,
+  Orders,
+  PaymentMethods,
+  Reviews,
 }
