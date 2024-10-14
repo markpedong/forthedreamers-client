0d2200d76e12cdc3fbae71fbbf7769c3d134b47f
markpedong <mark.pedong01@gmail.com>
2024-10-14 23:06:15 +0800

    middleware.

--- middleware.ts
+++ middleware.ts
@@ -1,14 +1,13 @@
-import { NextRequest } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 
-// This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
-  // const token = request.cookies.get('token')?.value
-  // if (!token) {
-  //   return NextResponse.redirect(new URL('/login', request.url))
-  // }
-  // return NextResponse.next()
+	const token = request.cookies.get('token')?.value;
+	if (!token) {
+		return NextResponse.redirect(new URL('/login', request.url));
+	}
+	return NextResponse.next();
 }
 
-// export const config = {
-//   matcher: ['/account', '/checkout'],
-// }
+export const config = {
+	matcher: ['/account', '/checkout'],
+};
